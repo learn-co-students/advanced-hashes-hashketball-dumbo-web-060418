023c443d7b5b5000fc60258d3555f676a46bfcfd
Jane Costa <yberdysh@gmail.com>
2018-06-23 20:03:13 -0400

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,94 @@
 # Write your code here!
 
 
+require 'pry'
+
+def game_hash
+	new_hash = {
+		:home => {},
+		:away => {}
+	}
+
+	# new_hash.each do |key, value|
+	# 	value.map do |hash|
+	# 		hash[:team_name] => "",
+	# 		hash[:colors] => [],
+	# 		hash[:players] => {}
+
+	# 	end
+	# end 
+	new_hash
+end
+
+	# player_names = ["Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"]
+	# player_numbers = [0, 30, 11, 1, 31]
+	# player_shoe = [16, 14, 17, 19, 15]
+	# player_points = [22, 12, 17, 26, 19]
+	# player_rebounds = [12, 12, 19, 12, 2]
+	# player_assists = [12, 12, 10, 6, 2]
+	# player_steals = [3, 12, 3, 3, 4]
+	# player_blocks = [1, 12, 1, 8, 11]
+	# player_slams = [1, 7, 15, 5, 1]
+
+	# away_names = ["Jeff Adrien", "Bismak Biyombo", "DeSagna Diop", "Ben Gordon", "Brendan Haywood"]
+	# away_numbers = [4, 0, 2, 8, 33]
+	# away_shoe = [18, 16, 14, 15, 15]
+	# away_points = [10, 12, 24, 33, 6]
+	# away_rebounds = [1, 4, 12, 3, 12]
+	# away_assists = [1, 7, 12, 2, 12]
+	# away_steals = [2, 7, 4, 1, 22]
+	# away_blocks = [7, 15, 5, 1, 5]
+	# away_slams = [2, 10, 5, 0, 12]
+
+	# (0..5).each do |i|
+	# 	new_hash[:home][:players][player_names[i]] = {
+	# 		:number => player_numbers[i],
+	# 		:shoe => player_shoe[i],
+	# 		:points => player_points[i],
+	# 		:rebounds => player_rebounds[i],
+	# 		:assists => player_assists[i],
+	# 		:steals => player_steals[i],
+	# 		:blocks => player_blocks[i],
+	# 		:slam_dunks => player_slams[i]
+	# 	}
+	# end 
+
+	# (0..5).each do |i|
+	# 	new_hash[:away][:players][away_names[i]] = {
+	# 		:number => away_numbers[i],
+	# 		:shoe => away_shoe[i],
+	# 		:points => away_points[i],
+	# 		:rebounds => away_rebounds[i],
+	# 		:assists => away_assists[i],
+	# 		:steals => away_steals[i],
+	# 		:blocks => away_blocks[i],
+	# 		:slam_dunks => away_slams[i]
+	# 	}
+	# end 
+	# new_hash
+# end
+
+# def num_points_scored	
+# 	game_hash
+# end
+
+# def team_colors
+# 	game_hash
+# end
+
+# def team_names
+# 	game_hash
+# end
 
 
+# def player_numbers
+# 	game_hash
+# end
 
+# def player_stats
+# 	game_hash
+# end
 
-
-
-
-
+# def big_shoe_rebounds
+# 	game_hash
+# end
